b049e01486e95967ea17b472e35536fafb6c7e35
Kiyoshi Ikehara <kiyoshi.ikehara@gree.net>
2014-02-06 15:12:30 +0900

    Add relaunchSettings to System.Prefork.Settings

--- src/System/Prefork/Settings.hs
+++ src/System/Prefork/Settings.hs
@@ -1,5 +1,5 @@
 
-module System.Prefork.Settings (defaultSettings) where
+module System.Prefork.Settings (defaultSettings, relaunchSettings) where
 
 import Control.Exception (SomeException, catch)
 import System.Posix
@@ -7,6 +7,11 @@ import System.Posix
 import System.Prefork.Class
 import System.Prefork.Types
 import System.Prefork.Worker
+import Control.Concurrent.STM
+import qualified Data.Set as S
+import qualified Data.Map as M
+import Data.List
+import Control.Monad
 
 defaultSettings :: PreforkSettings sc
 defaultSettings = PreforkSettings {
@@ -21,8 +26,46 @@ defaultSettings = PreforkSettings {
   , psUpdateConfig     = return (Nothing)
   }
 
+relaunchSettings :: (Ord w, Eq w) => PreforkResource w -> (w -> IO (ProcessID)) -> PreforkSettings sc
+relaunchSettings resouce forkAction = defaultSettings {
+      -- psUpdateConfig = updateConfig
+      psUpdateServer = updateWorkers resouce forkAction
+    , psCleanupChild = cleanupChild resouce
+    , psOnChildFinished = relaunchWorkers resouce forkAction
+  }
+
+-- Clean up application specific resources associated to a child process
+cleanupChild :: (Ord w, Eq w) => PreforkResource w -> sc -> ProcessID -> IO ()
+cleanupChild resource _config pid = atomically $ modifyTVar' (prProcs resource) $ M.delete pid
+
+-- Update the entire state of a server
+updateWorkers :: (Ord w, Eq w) => PreforkResource w -> (w -> IO (ProcessID)) -> sc -> IO ([ProcessID])
+updateWorkers resource forkAction _config = do
+  workers <- readTVarIO (prWorkers resource)
+  newPids <- forM (S.toList workers) $ \w -> do
+    pid <- forkAction w
+    return (pid, w)
+  oldPids <- atomically $ swapTVar (prProcs resource) (M.fromList newPids)
+  forM_ (M.keys oldPids) $ sendSignal sigTERM
+  return (map fst newPids)
+
+-- Relaunch workers after some of them terminate
+relaunchWorkers :: (Ord w, Eq w) => PreforkResource w -> (w -> IO (ProcessID)) -> sc -> IO ([ProcessID])
+relaunchWorkers resource forkAction _config = do
+  let procs = prProcs resource
+  let workers = prWorkers resource
+  live <- readTVarIO procs
+  workers <- readTVarIO workers
+  newPids <- fmap M.fromList $ forM (S.toList workers \\ M.elems live) $ \w -> do
+    pid <- forkAction w
+    -- pid <- forkWorkerProcessWithArgs w ["id=" ++ show (wId w) ]
+    return (pid, w)
+  atomically $ modifyTVar' procs $ M.union newPids
+  return (M.keys newPids)
+
 sendSignal :: Signal -> ProcessID -> IO ()
 sendSignal sig cid = signalProcess sig cid `catch` ignore
   where
     ignore :: SomeException -> IO ()
     ignore _ = return ()
+
